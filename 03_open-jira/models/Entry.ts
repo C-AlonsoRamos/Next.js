7b264b6ec02cc0bfdfb1a120641ae3f44e7850c0
Cristian Alonso <christianalonso.r10@gmail.com>
2023-08-28 17:26:48 +0200

    models database

---
+++ 03_open-jira/models/Entry.ts
@@ -0,0 +1,21 @@
+import { Entry } from "@/interfaces";
+import mongoose, { Model, Schema } from "mongoose";
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, require: true },
+  createdAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ["pending", "in-progres", "finished"],
+      message: "{VALUE} no es un estado permitido ",
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model("Entry", entrySchema);
+
+export default EntryModel;
